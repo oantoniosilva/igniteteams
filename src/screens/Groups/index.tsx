7c651e6db02b808d56678679a01383326fe6c301
antonio <antoniopsilva083@gmail.com>
2022-10-05 00:17:30 -0300

    feat: create groupcard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
@@ -6,9 +7,14 @@ export function Groups() {
   return (
     <Container>      
       <Header />
+
       <Highlight 
         title="Turmas"
         subtitle="Jogue com a sua turma"
+      />
+
+      <GroupCard 
+        title="Galera do Ignite"
       />
     </Container>
   )    

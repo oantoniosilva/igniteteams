631e95cd10209cd7290994236a40e7c8adccc633
antonio <antoniopsilva083@gmail.com>
2022-10-04 23:32:14 -0300

    feat: create highlight component

---
+++ src/components/Highlight/index.tsx
@@ -0,0 +1,21 @@
+import { Container } from "@components/Loading/styles";
+import { Subtitle, Title } from "./styles";
+
+type Props = {
+  title: string;
+  subtitle: string;
+}
+
+export function Highlight({ title, subtitle }: Props) {
+  return (
+    <Container>
+      <Title>
+        {title}
+      </Title>
+
+      <Subtitle>
+        {subtitle}
+      </Subtitle>
+    </Container>
+  )
+}

6110d78ce60407853085e2142c5b17b178d326ab
Antonio <antoniopsilva083@gmail.com>
2022-10-09 15:53:07 -0300

    feat: create button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@components/Button";
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -32,6 +33,10 @@ export function Groups() {
             message="Que tal cadastrar a primeira turma?" 
           />
         )}  
+      />
+
+      <Button 
+        title="Criar nova turma"
       />
     </Container>
   )    
